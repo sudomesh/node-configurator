a98f00cbd6c234d9f0aef44d6672aa9444f40d11
rhodey <rhodey@anhonesteffort.org>
2013-10-10 11:31:51 -0700

    moved WebSocket client code into the existing config.js file.

--- static_web/js/config.js
+++ static_web/js/config.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
   console.log("mesh the planet!");
+  
+  var sock = null;
+  var wsuri = "ws://localhost:9000";
+  
+  sock = new WebSocket(wsuri);
+  
+  function send(msg) {
+    sock.send(msg);
+  };
+  
+  sock.onopen = function() {
+    console.log("connected to " + wsuri);
+  }
+ 
+  sock.onclose = function(e) {
+    console.log("connection closed (" + e.code + ")");
+  }
+  
+  sock.onmessage = function(e) {
+    console.log("message received: " + e.data);
+  }
 });
