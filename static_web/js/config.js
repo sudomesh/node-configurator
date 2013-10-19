3b92811204101bacef57ce434d8903388bf0a870
Marc Juul <juul@labitat.dk>
2013-10-18 17:13:34 -0700

    made the js prettier and object oriented'er

--- static_web/js/config.js
+++ static_web/js/config.js
@@ -1,82 +1,78 @@
-var CONST_INTERVAL_MS = 5000;
+/*
 
-var last_timer;
-var wsuri = "wss://localhost:8080/websocket";
-var sock = new WebSocket(wsuri);
+  This is the javascript library for the 
+  sudomesh node configuration web app.
 
-function update_node_list(data) {
-    var node = data.node_obj;
-    var outer = document.createElement('DIV');
-    outer.id = 'node_0';
-    outer.className = 'node';
-    var sub = document.createElement('DIV');
-    sub.className = 'title';
-    sub.innerHTML = node.hardware_model || "Unknown";
-    outer.appendChild(sub);
+  License: AGPL
 
-    sub = document.createElement('DIV');
-    sub.className = 'MAC';    
-    var subsub = document.createElement('SPAN');
-    subsub.className = 'label';
-    subsub.innerHTML = "MAC:";
-    sub.appendChild(subsub);
-    subsub = document.createElement('SPAN');
-    subsub.className = 'text';
-    subsub.innerHTML = "C0:DE:CA:FE:C0:CA:FE";
-    sub.appendChild(subsub);
-    outer.appendChild(sub);
+*/
 
-    sub = document.createElement('DIV');
-    sub.className = 'status';    
-    var subsub = document.createElement('SPAN');
-    subsub.className = 'label';
-    subsub.innerHTML = "Status::";
-    sub.appendChild(subsub);
-    subsub = document.createElement('SPAN');
-    subsub.className = 'text';
-    subsub.innerHTML = "Unconfigured";
-    sub.appendChild(subsub);
-    outer.appendChild(sub);
 
-    $('#node_list').html('');
-    $('#node_list')[0].appendChild(outer);
-}
+var NodeConf = {
+
+    wsuri: "wss://localhost:8080/websocket",
+    last_timer: null,
+    CONST_INTERVAL_MS: 5000,
+    sock: null,
+    node_template: null,
+
+    init: function() {
+
+        this.sock = new WebSocket(this.wsuri);
+        this.node_template = _.template($('#node_template').html());
+
+        console.log("mesh the planet!");
   
-function send_command() {
-  fakeNode = {
-    "hardware_model"   : "Ubiquity nano-station",
-    "firmware_version" : "SudoNode v0.5",
-    "geo_location"     : "Oakland, CA",
-    "op_name"          : "A Cool Person",
-    "op_email"         : "coolpeer@idk.org",
-    "op_phone"         : "1-555-555-1337"
-  };
-  
-  fakeCommand = {
-    "command"   : "node::set_config",
-    "socket_id" : Math.floor(Math.random() * 100),
-    "node_obj"  : fakeNode
-  };
-  
-  sock.send(JSON.stringify(fakeCommand));
-}
-
-$(document).ready(function() {
-  console.log("mesh the planet!");
-  
-  sock.onopen = function() {
-    console.log("connected to " + wsuri);
-  }
+        this.sock.onopen = function() {
+            console.log("connected to " + this.wsuri);
+        }.bind(this);
  
-  sock.onclose = function(e) {
-    console.log("connection closed (" + e.code + ")");
-  }
+        this.sock.onclose = function(e) {
+            console.log("connection closed (" + e.code + ")");
+        }.bind(this);
   
-  sock.onmessage = function(e) {
-    console.log("message received: " + e.data);
-      var data = $.parseJSON(e.data);
-    update_node_list(data);
+        this.sock.onmessage = function(e) {
+            console.log("message received: " + e.data);
+            var data = $.parseJSON(e.data);
+            // TODO fix
+            data = [data];
+            this.update_node_list(data);
+        }.bind(this);
+    },
+    
+    update_node_list: function(nodes) {
 
-//    send_command();
-  }
-});
+        var container = $('#node_list');
+        var i, h;
+        for(i=0; i < nodes.length; i++) {
+            // TODO fix
+            nodes[i].id = 'node_'+i;
+            nodes[i].title = nodes[i].hardware_model;
+            nodes[i].status = 'unconfigured';
+            nodes[i].mac = 'C0:DE:CA:FE:C0:DE:CA:FE';
+            h = this.node_template(nodes[i]);
+            container.append(h);
+        }
+    },
+
+    send_command: function() {
+        fakeNode = {
+            "hardware_model"   : "Ubiquity nano-station",
+            "firmware_version" : "SudoNode v0.5",
+            "geo_location"     : "Oakland, CA",
+            "op_name"          : "A Cool Person",
+            "op_email"         : "coolpeer@idk.org",
+            "op_phone"         : "1-555-555-1337"
+        };
+        
+        fakeCommand = {
+            "command"   : "node::set_config",
+            "socket_id" : Math.floor(Math.random() * 100),
+            "node_obj"  : fakeNode
+        };
+        
+        sock.send(JSON.stringify(fakeCommand));
+    }
+};
+
+$(document).ready(NodeConf.init.bind(NodeConf));
