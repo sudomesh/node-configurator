d4be5bb2d4651b9d480a326de50d29b25bf7d11e
Marc Juul <juul@labitat.dk>
2013-12-19 22:38:22 -0800

    added zeroconf module

---
+++ zeroconfService.py
@@ -0,0 +1,38 @@
+
+import dbus
+import avahi
+
+
+class ZeroconfService:
+
+    def __init__(self, name, port, stype="_nodeconf._tcp",
+                 domain="", host="", text="http://sudomesh.org"):
+        self.name = name
+        self.stype = stype
+        self.domain = domain
+        self.host = host
+        self.port = port
+        self.text = text
+
+    def publish(self):
+        bus = dbus.SystemBus()
+        server = dbus.Interface(
+                         bus.get_object(
+                                 avahi.DBUS_NAME,
+                                 avahi.DBUS_PATH_SERVER),
+                        avahi.DBUS_INTERFACE_SERVER)
+
+        g = dbus.Interface(
+                    bus.get_object(avahi.DBUS_NAME,
+                                   server.EntryGroupNew()),
+                    avahi.DBUS_INTERFACE_ENTRY_GROUP)
+
+        g.AddService(avahi.IF_UNSPEC, avahi.PROTO_UNSPEC,dbus.UInt32(0),
+                     self.name, self.stype, self.domain, self.host,
+                     dbus.UInt16(self.port), self.text)
+
+        g.Commit()
+        self.group = g
+
+    def unpublish(self):
+        self.group.Reset()
