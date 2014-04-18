e2ed542c135017b00545c5530a64801fa23dd659
Marc Juul <juul@labitat.dk>
2014-04-18 05:23:41 -0700

    changed 5ghz channel

--- postscripts/01-wireless.sh
+++ postscripts/01-wireless.sh
@@ -4,7 +4,7 @@
 # http://wiki.openwrt.org/doc/uci/wireless
 
 uci set wireless.@wifi-device[0].htmode='HT40+'
-uci set wireless.@wifi-device[0].channel=161
+uci set wireless.@wifi-device[0].channel=157
 uci delete wireless.@wifi-device[0].disabled
 
 uci delete wireless.@wifi-iface[0]
